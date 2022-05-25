19e45827cdeaf95cf1d425e7db201d1943a3b5ce
Drago Vrban <drago.vrban@jsguru.io>
2022-05-25 12:13:09 +0200

    initial commit

---
+++ packages/components/stencil.config.ts
@@ -0,0 +1,27 @@
+import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+
+export const config: Config = {
+  namespace: 'components',
+  outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@jsguru/components',
+      proxiesFile: '../components-react/src/components.ts',
+    }),
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
